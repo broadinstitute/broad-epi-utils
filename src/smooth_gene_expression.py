485624025f608cc25a9e82ca474b00503b29e729
Siddarth Wekhande <42479997+sidwekhande@users.noreply.github.com>
2025-04-16 09:07:38 -0400

    - add parameterized function

--- src/smooth_gene_expression.py
+++ src/smooth_gene_expression.py
@@ -42,3 +42,59 @@ adata.layers["X_smoothed"] = X_smoothed
 
 # 4. Access the smoothed expression for a specific gene
 adata[:, gene].layers["X_smoothed"]
+
+
+
+# parameterized function to use with scanpy/snapatac2
+
+def smooth_layer_from_knn(adata, genes=None, layer_name="smooth", sigma=None, n_neighbors=None):
+    """
+    Smooth gene expression using KNN with Gaussian kernel from precomputed distances.
+
+    Parameters
+    ----------
+    adata : AnnData
+        Must have `obsp["distances"]` populated.
+    genes : list or None
+        List of gene names to smooth. If None, smooth all genes.
+    layer_name : str
+        Name of the new layer to store smoothed matrix in AnnData.
+    sigma : float or None
+        Bandwidth for Gaussian kernel. If None, uses mean of non-zero distances.
+    n_neighbors : int or None
+        Number of neighbors to keep. If None, use all neighbors. This takes a while to compute for all neighbors - suggestion is take to closest 50.
+    """
+
+    D = adata.obsp["distances"].tocsr() 
+
+    if sigma is None:
+        sigma = np.mean(D.data)
+
+    W = D.copy()
+    W.data = np.exp(-W.data**2 / (2 * sigma**2))
+
+    # keep only top n_neighbors if specified
+    if n_neighbors is not None:
+        for i in range(W.shape[0]): #for every row, take the first and last non-zero value, sort, set sorted neighbors distance past n_neighbors to 0, and then remove them
+            row_start = W.indptr[i]
+            row_end = W.indptr[i+1]
+            row_data = W.data[row_start:row_end]
+            if len(row_data) > n_neighbors:
+                idx = np.argsort(row_data)[-n_neighbors:]
+                mask = np.zeros_like(row_data, dtype=bool)
+                mask[idx] = True
+                W.data[row_start:row_end][~mask] = 0
+        W.eliminate_zeros()
+
+    row_sums = np.array(W.sum(axis=1)).flatten()
+    W = W.multiply(1 / row_sums[:, np.newaxis])
+
+    X = adata[:, genes].X if genes is not None else adata.X
+    if not sp.issparse(X):
+        X = sp.csr_matrix(X)
+
+    #Compute smoothed gene expression
+    X_smooth = W @ X
+
+    # Save as new layer in AnnData object
+    adata.layers[layer_name] = X_smooth
